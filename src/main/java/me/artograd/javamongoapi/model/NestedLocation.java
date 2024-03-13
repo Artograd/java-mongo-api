8ea4e418cf0371c406f9da92b51562c65e7d2ff7
Maksim-Totski <Maksim.Totski@gmail.com>
2024-03-13 12:03:39 +0100

    added ID and docs to nested location

--- src/main/java/me/artograd/javamongoapi/model/NestedLocation.java
+++ src/main/java/me/artograd/javamongoapi/model/NestedLocation.java
@@ -1,11 +1,22 @@
 package me.artograd.javamongoapi.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema
 public class NestedLocation {
+	
+	@Schema(description = "Id of location entity")
+	private String id;
+	
+	@Schema(description = "name of location entity")
 	private String name;
+	
+	@Schema(description = "nested child")
 	private NestedLocation child;
 	
-	public NestedLocation(String name, NestedLocation child) {
+	public NestedLocation(String id, String name, NestedLocation child) {
 		super();
+		this.id = id;
 		this.name = name;
 		this.child = child;
 	}
@@ -21,5 +32,11 @@ public class NestedLocation {
 	}
 	public void setChild(NestedLocation child) {
 		this.child = child;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
 	}
 }
